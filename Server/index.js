65a5590323920c37dfaf7058350845db5993bcc2
Elizabeth Mendenhall <eamendenhall@outlook.com>
2021-06-04 14:48:23 -0700

    set up server

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('./../Client/dist'));
+
+app.listen(port, () => {
+  console.log('Connected to server at port', 3000);
+});
