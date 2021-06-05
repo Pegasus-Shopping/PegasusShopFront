59f0c96f9faa49b31317891cb7a0d2149f3e6243
Alexander Causey <alexander.w.causey@gmail.com>
2021-06-04 17:02:28 -0700

    created app component

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./Component/App.jsx";
 
-ReactDOM(<div>hello world</div>, document.getElementById("root"));
+ReactDOM(<App />, document.getElementById("root"));
