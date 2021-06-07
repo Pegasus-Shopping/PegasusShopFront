b513f3414c062b62420c71a5e2b6aaa87189793a
Elizabeth Mendenhall <eamendenhall@outlook.com>
2021-06-07 11:18:44 -0700

    set up basic file structure and top level components for modules, added resolve extensions to ck (not working yet!)

--- Client/src/Components/App.jsx
+++ Client/src/Components/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+/* eslint-disable import/extensions */
+import React from "react";
+import ProductOverview from "./product-overview/ProductOverview.jsx";
+import RelatedProductsComparison from "./related-products/RelatedProductsComparison.jsx";
+import QuestionsAnswers from "./questions-answers/QuestionsAnswers.jsx";
+import ProductReviews from "./product-reviews/ProductReviews.jsx";
 
-function App(props) {
+function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Shopping</h1>
+      <ProductOverview />
+      <RelatedProductsComparison />
+      <QuestionsAnswers />
+      <ProductReviews />
+    </div>
   );
 }
 
