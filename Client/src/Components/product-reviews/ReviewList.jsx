f20daea784bf032e7fcf4ae7998f713f8ea68934
Alex Ting <alexting6366@gmail.com>
2021-06-09 16:44:56 -0700

    Added sort by relevance functionality.

--- Client/src/Components/product-reviews/ReviewList.jsx
+++ Client/src/Components/product-reviews/ReviewList.jsx
@@ -12,16 +12,34 @@ function ReviewList({ list }) {
   const helpfulList = [...listNewDateFormat].sort((a, b) => b.helpfulness - a.helpfulness);
   const latestList = [...listNewDateFormat].sort((a, b) => b.date - a.date);
 
+  // Assigns variable relevantValue to each object in review list and then returns a list
+  // sorted by relevantValue. relevantValue = 2 * helpfullCount - (current date - review date).
+  // current date - review date is measured in days
+  function createRelevant() {
+    const tempList = [...listNewDateFormat];
+    const date = new Date();
+    const currentDateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    const currentDate = new Date(currentDateString);
+    const relevantList = tempList.map((review) => {
+      const relevantValue = 2 * review.helpfulness
+        - ((currentDate.getTime() - review.date.getTime()) / (1000 * 3600 * 24));
+      return { ...review, relevantValue };
+    });
+    return relevantList.sort((a, b) => b.relevantValue - a.relevantValue);
+  }
+
   function onChange(event) {
     setList(event.target.value);
   }
+
+  const relevantList = createRelevant();
 
   return (
     <div>
       <select id="test" onChange={onChange}>
         <option value="Latest">Latest</option>
         <option value="Most Helpful">Most Helpful</option>
-        <option value="recommended">recommended</option>
+        <option value="Relevant">Relevant</option>
       </select>
       <>
         {listCounter === "Latest" && (latestList.map((review) => (
@@ -32,7 +50,7 @@ function ReviewList({ list }) {
         )))}
       </>
       <>
-        {listCounter === "recommended" && (listNewDateFormat.map((review) => (
+        {listCounter === "Relevant" && (relevantList.map((review) => (
           <>
             <Review review={review} />
             <hr />
