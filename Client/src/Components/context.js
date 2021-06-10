96b8092a23d6a9cea493e3cd28034011447a3d08
Alex Ting <alexting6366@gmail.com>
2021-06-10 16:10:56 -0700

    Revert "Merge branch 'main' of https://github.com/Pegasus-Shopping/PegasusShopFront into Alex-Branch"
    This reverts commit e5ba00e3f60382ef0d573ae647e5b46077852bf8, reversing
    changes made to 9fe63e55547254d1fc30ae240a4649a45ebf1ddc.

--- Client/src/Components/context.js
+++ Client/src/Components/context.js
@@ -2,86 +2,85 @@ import React from "react";
 
 const DataContext = React.createContext(
   {
-
-    product: {
-      id: 11,
-      name: "Air Minis 250",
-      slogan: "Full court support",
-      description: "This optimized air cushion...",
-      category: "Basketball Shoes",
-      default_price: "0",
-      features: [
-        {
-          feature: "Sole",
-          value: "Rubber",
-        },
-        {
-          feature: "Material",
-          value: "FullControlSkin",
-        },
-      ],
-    },
-    styles: [
+  product: {
+    id: 11,
+    name: "Air Minis 250",
+    slogan: "Full court support",
+    description: "This optimized air cushion...",
+    category: "Basketball Shoes",
+    default_price: "0",
+    features: [
       {
-        style_id: 1,
-        name: "Forest Green & Black",
-        original_price: "140",
-        sale_price: "0",
-        "default?": true,
-        photos: [
-          {
-            thumbnail_url: "urlplaceholder/style_1_photo_number_thumbnail.jpg",
-            url: "urlplaceholder/style_1_photo_number.jpg",
-          },
-          {
-            thumbnail_url: "urlplaceholder/style_1_photo_number_thumbnail.jpg",
-            url: "urlplaceholder/style_1_photo_number.jpg",
-          },
-        ],
-        skus: {
-          37: {
-            quantity: 8,
-            size: "XS",
-          },
-          38: {
-            quantity: 16,
-            size: "S",
-          },
-          39: {
-            quantity: 17,
-            size: "M",
-          },
-        },
+        feature: "Sole",
+        value: "Rubber",
       },
       {
-        style_id: 2,
-        name: "Desert Brown & Tan",
-        original_price: "140",
-        sale_price: "0",
-        "default?": false,
-        photos: [
-          {
-            thumbnail_url: "urlplaceholder/style_2_photo_number_thumbnail.jpg",
-            url: "urlplaceholder/style_2_photo_number.jpg",
-          },
-        ],
-        skus: {
-          37: {
-            quantity: 8,
-            size: "XS",
-          },
-          38: {
-            quantity: 16,
-            size: "S",
-          },
-          39: {
-            quantity: 17,
-            size: "M",
-          },
-        },
+        feature: "Material",
+        value: "FullControlSkin",
       },
     ],
-    styleIndex: 0,
+  },
+  styles: [
+    {
+      style_id: 1,
+      name: "Forest Green & Black",
+      original_price: "140",
+      sale_price: "0",
+      "default?": true,
+      photos: [
+        {
+          thumbnail_url: "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+          url: "urlplaceholder/style_1_photo_number.jpg",
+        },
+        {
+          thumbnail_url: "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+          url: "urlplaceholder/style_1_photo_number.jpg",
+        },
+      ],
+      skus: {
+        37: {
+          quantity: 8,
+          size: "XS",
+        },
+        38: {
+          quantity: 16,
+          size: "S",
+        },
+        39: {
+          quantity: 17,
+          size: "M",
+        },
+      },
+    },
+    {
+      style_id: 2,
+      name: "Desert Brown & Tan",
+      original_price: "140",
+      sale_price: "0",
+      "default?": false,
+      photos: [
+        {
+          thumbnail_url: "urlplaceholder/style_2_photo_number_thumbnail.jpg",
+          url: "urlplaceholder/style_2_photo_number.jpg",
+        },
+      ],
+      skus: {
+        37: {
+          quantity: 8,
+          size: "XS",
+        },
+        38: {
+          quantity: 16,
+          size: "S",
+        },
+        39: {
+          quantity: 17,
+          size: "M",
+        },
+      },
+    },
+  ],
+  styleIndex: 0,
   },
 );
 
