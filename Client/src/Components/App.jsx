107fd9556a9fb2a83cad4fd872d247f0d404d027
Alex Ting <alexting6366@gmail.com>
2021-06-16 15:04:09 -0700

    Changed axios requets to axios.all to speed up website loading process.

--- Client/src/Components/App.jsx
+++ Client/src/Components/App.jsx
@@ -16,28 +16,19 @@ function App() {
   const [styleIndex, setStyleIndex] = useState(0);
 
   useEffect(() => {
-    // get styles
-    axios.get(`/products/${id}/styles`)
-      .then((res) => {
-        setStyles(res.data.results);
-      })
-      .then( // get ratings
-        () => axios.get("/reviews/meta/", { params: { product_id: id } })
-          .then((res) => {
-            setRating(res.data.ratings);
-          }),
-      ).then(
-        () => ( // get details
-          axios.get(`/products/${id}`)
-            .then((res) => {
-              // console.log(res);
-              setProduct(res.data);
-            })
-        ),
-      )
-      .then(() => {
-        setBusy(false);
-      });
+    axios.all([
+      axios.get(`/products/${id}/styles`),
+      axios.get("/reviews/meta/", { params: { product_id: id } }),
+      axios.get(`/products/${id}`),
+    ])
+      .then(
+        axios.spread((stylesReturn, ratingReturn, productReturn) => {
+          setStyles(stylesReturn.data.results);
+          setRating(ratingReturn.data.ratings);
+          setProduct(productReturn.data);
+          setBusy(false);
+        }),
+      );
   }, [id]);
   const updateCount = () => {
     setOutfitCount(outfitCount + 1);
