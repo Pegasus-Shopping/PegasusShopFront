00813cfc6a66a9c7586e29165d4172177af92d2d
Alexander Causey <alexander.w.causey@gmail.com>
2021-06-16 12:12:36 -0700

    added click listeners

--- Client/src/Components/product-overview/ImageExpander.jsx
+++ Client/src/Components/product-overview/ImageExpander.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import DataContext from "../context";
 import css from "./styles.css";
 import { left, right } from "./helper-functions";
+import RecordClicks from "../RecordClicks";
 
 function ImageExpander({ thumb, setThumb, toggleExpanded }) {
   const { styles, styleIndex } = useContext(DataContext);
@@ -19,13 +20,21 @@ function ImageExpander({ thumb, setThumb, toggleExpanded }) {
   };
   return (
     <div className={css.expandedGrid}>
-      <button type="button" className={css.exitExpander} onClick={() => toggleExpanded(false)}><i aria-label="Close expanded image" className="far fa-times-circle" /></button>
-      <button type="button" className={css.chevronLeftExpander} onClick={() => setThumb(left(thumb, styles.length - 1))}><i aria-label="Toggle left" className="fas fa-chevron-left" /></button>
-      <button type="button" className={css.chevronRightExpander} onClick={() => setThumb(right(thumb, styles.length - 1))}><i aria-label="Toggle right" className="fas fa-chevron-right" /></button>
-      <input type="checkbox" id="zoom" className={css.zoomIn} />
-      <label htmlFor="zoom" className={css.expandedImage}>
-        <img onMouseOver={moveZoom} onMouseMove={moveZoom} onFocus={moveZoom} id="image" src={url} className={css.expandedImage} alt={styles[styleIndex].name} />
-      </label>
+      <RecordClicks widget="product overview" element="exit expander">
+        <button type="button" className={css.exitExpander} onClick={() => toggleExpanded(false)}><i aria-label="Close expanded image" className="far fa-times-circle" /></button>
+      </RecordClicks>
+      <RecordClicks widget="product overview" element="toggle left expander">
+        <button type="button" className={css.chevronLeftExpander} onClick={() => setThumb(left(thumb, styles.length - 1))}><i aria-label="Toggle left" className="fas fa-chevron-left" /></button>
+      </RecordClicks>
+      <RecordClicks widget="product overview" element="toggle right expander">
+        <button type="button" className={css.chevronRightExpander} onClick={() => setThumb(right(thumb, styles.length - 1))}><i aria-label="Toggle right" className="fas fa-chevron-right" /></button>
+      </RecordClicks>
+      <RecordClicks widget="product overview" element="toggle zoom">
+        <input type="checkbox" id="zoom" className={css.zoomIn} />
+        <label htmlFor="zoom" className={css.expandedImage}>
+          <img onMouseOver={moveZoom} onMouseMove={moveZoom} onFocus={moveZoom} id="image" src={url} className={css.expandedImage} alt={styles[styleIndex].name} />
+        </label>
+      </RecordClicks>
     </div>
   );
 }
